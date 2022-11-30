94844b43426983c2842252c70fdc1bb17c02d5bb
jose julian vargas potes <jjvargas.potes@gmail.com>
2022-11-30 13:09:05 -0500

    create task_schema

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 var express = require("express");
+var TaskModel = require('./task_schema');
 var router = express.Router();
 var query = "mongodb+srv://jose:Killerdark2@cluster0.f6ystdv.mongodb.net/taskBD?retryWrites=true&w=majority"
 const db = (query);
