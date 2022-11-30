6a78a8bce4453f6c6b0d2b9d7a93a63b813d2872
jose julian vargas potes <jjvargas.potes@gmail.com>
2022-11-30 12:52:48 -0500

    Add . gitinore, add server, add, add package.json and add server.js

---
+++ api.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+var express = require("express");
+var router = express.Router();
+var query = "mongodb+srv://jose:Killerdark2@cluster0.f6ystdv.mongodb.net/taskBD?retryWrites=true&w=majority"
+const db = (query);
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, function (error) {
+    if (error) {
+        console.log("Error!" + error);
+    } else {
+        console.log("Se ha conectado con la base de datos exitosamente");
+    }
+});
+
+module.exports = router;
